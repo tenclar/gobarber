4e6dee13a73a5f9969ccca3038c60f00003486f1
Tenclar <tenclarvalus@gmail.com>
2020-06-29 22:05:33 -0500

    teste Update

--- api/src/modules/users/services/UpdateUserAvatarService.spec.ts
+++ api/src/modules/users/services/UpdateUserAvatarService.spec.ts
@@ -26,4 +26,51 @@ describe('UpdateUserAvatar', () => {
 
     expect(user.avatar).toBe('avatar.jpg');
   });
+
+  it('should be able to create a new user', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeStorageProvider = new FakeStorageProvider();
+    const UpdateUserAvatar = new UpdateUserAvatarService(
+      fakeUsersRepository,
+      fakeStorageProvider,
+    );
+
+    expect(
+      UpdateUserAvatar.execute({
+        user_id: 'non-existing-user',
+        avatarFilename: 'avatar.jpg',
+      }),
+    ).toBeInstanceOf(AppError);
+  });
+
+  it('should delete old avatar when updating new one', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeStorageProvider = new FakeStorageProvider();
+
+    const deleteFile = jest.spyOn(fakeStorageProvider, 'deleteFile');
+
+    const UpdateUserAvatar = new UpdateUserAvatarService(
+      fakeUsersRepository,
+      fakeStorageProvider,
+    );
+
+    const user = await fakeUsersRepository.create({
+      name: 'Tenclar Valus',
+      email: 'tenclarvalus@gmail.com',
+      password: '123456',
+    });
+
+    await UpdateUserAvatar.execute({
+      user_id: user.id,
+      avatarFilename: 'avatar.jpg',
+    });
+
+    await UpdateUserAvatar.execute({
+      user_id: user.id,
+      avatarFilename: 'avatar2.jpg',
+    });
+
+    expect(deleteFile).toHaveBeenCalledWith('avatar.jpg');
+    expect(user.avatar).toBe('avatar.jpg');
+  });
 });
