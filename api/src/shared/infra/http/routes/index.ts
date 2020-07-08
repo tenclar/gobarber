ce59321788281adab7db99f24e09c6bd880e09ba
Tenclar <tenclarvalus@gmail.com>
2020-07-07 21:14:16 -0500

    rotas

--- api/src/shared/infra/http/routes/index.ts
+++ api/src/shared/infra/http/routes/index.ts
@@ -2,10 +2,11 @@ import { Router } from 'express';
 import appointmentsRouter from '@modules/appointments/infra/http/routes/appointments.routes';
 import usersRouter from '@modules/users/infra/http/routes/user.routes';
 import sessionsRouter from '@modules/users/infra/http/routes/sessions.routes';
+import passwordRouter from '@modules/users/infra/http/routes/passowords.routes';
 
 const routes = Router();
 routes.use('/sessions', sessionsRouter);
 routes.use('/appointments', appointmentsRouter);
-
+routes.use('/password', passwordRouter);
 routes.use('/users', usersRouter);
 export default routes;
