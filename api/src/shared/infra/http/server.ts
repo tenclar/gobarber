161301177fbd357762d8a336b9f9711ed0894187
Tenclar <tenclarvalus@gmail.com>
2020-07-13 21:49:53 -0500

    retirada emoji

--- api/src/shared/infra/http/server.ts
+++ api/src/shared/infra/http/server.ts
@@ -31,5 +31,5 @@ app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
   });
 });
 app.listen(3333, () => {
-  console.log('🚀 server started on port 3333!');
+  console.log(' ;-) server started on port 3333!');
 });
