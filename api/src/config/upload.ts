7a510ed2ee3861321c1caac943cd155ef3a4b9a4
tenclar <tenclarvalus@gmail.com>
2020-07-15 18:40:22 -0500

    Cache com redis

--- api/src/config/upload.ts
+++ api/src/config/upload.ts
@@ -15,30 +15,28 @@ interface IUploadConfig {
     disk: {};
     aws: {
       bucket: string;
-    }
     };
   };
 }
+
 export default {
   driver: process.env.STORAGE_DRIVER,
   tmpFolder,
   uploadsFolder: path.resolve(tmpFolder, 'uploads'),
 
   multer: {
-
-      storage: multer.diskStorage({
-        destination: tmpFolder,
-        filename(request, file, callback) {
-          const fileHash = crypto.randomBytes(10).toString('HEX');
-          const fileName = `${fileHash}-${file.originalname}`;
-          return callback(null, fileName);
-        },
-      }),
-
+    storage: multer.diskStorage({
+      destination: tmpFolder,
+      filename(request, file, callback) {
+        const fileHash = crypto.randomBytes(10).toString('HEX');
+        const fileName = `${fileHash}-${file.originalname}`;
+        return callback(null, fileName);
+      },
+    }),
   },
 
   config: {
-    disk : {},
-    aws: { bucket: 'app-gobarber-2'},
-  }
+    disk: {},
+    aws: { bucket: 'app-gobarber-2' },
+  },
 } as IUploadConfig;
