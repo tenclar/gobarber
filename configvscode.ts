857fb0dc4466c371b822f8c1162470b5fa456ae5
tenclar <tenclarvalus@gmail.com>
2020-07-19 23:13:38 -0500

    configuração insomina vccode

---
+++ configvscode.ts
@@ -0,0 +1,14 @@
+  "material-icon-theme.folders.associations": {
+
+        "infra": "app",
+        "entities":"class",
+        "schemas": "class",
+        "typeorm": "database",
+        "repositories":"mappings",
+        "http":"container",
+        "migrations": "tools",
+        "modules":"components",
+        "implementations": "core",
+        "dtos": "typescript",
+        "fakes":"mock"
+    },
