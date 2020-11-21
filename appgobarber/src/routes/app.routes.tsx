0934fc571f738a12dec87fb093cff4d5537652f1
tenclar <tenclarvalus@gmail.com>
2020-11-21 10:45:34 -0500

    rottas

--- appgobarber/src/routes/app.routes.tsx
+++ appgobarber/src/routes/app.routes.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 
+import CreateAppointment from '../pages/CreateAppointment';
 import Dashboard from '../pages/Dashboard';
 import Profile from '../pages/Profile';
+import AppointmentCreated from '../pages/AppointmentCreated';
 
 const App = createStackNavigator();
 const AppRoutes: React.FC = () => (
@@ -14,6 +16,8 @@ const AppRoutes: React.FC = () => (
   >
     <App.Screen name="Dashboard" component={Dashboard} />
     <App.Screen name="Profile" component={Profile} />
+    <App.Screen name="CreateAppointment" component={CreateAppointment} />
+    <App.Screen name="AppointmentCreated" component={AppointmentCreated} />
   </App.Navigator>
 );
 
