5c4ce1f97b3261df37ac1b1039bec00c7333e28d
tenclar <tenclarvalus@gmail.com>
2020-11-21 07:35:38 -0500

    rota para profile

--- appgobarber/src/routes/app.routes.tsx
+++ appgobarber/src/routes/app.routes.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import Dashboard from '../pages/Dashboard';
+import Profile from '../pages/Profile';
 
 const App = createStackNavigator();
 const AppRoutes: React.FC = () => (
@@ -12,6 +13,7 @@ const AppRoutes: React.FC = () => (
     }}
   >
     <App.Screen name="Dashboard" component={Dashboard} />
+    <App.Screen name="Profile" component={Profile} />
   </App.Navigator>
 );
 
